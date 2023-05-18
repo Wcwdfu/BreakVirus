3cc905cfd0c88844d028b78d84af1b1e9e077802
ksk0823 <machine0823@naver.com>
2023-05-18 14:40:07 +0900

    이미지와 효과음 추가
    이미지 수정 + 추가
    효과음 추가

--- js/tutorial.js
+++ js/tutorial.js
@@ -1,22 +1,56 @@
-const comicPages = ["img/tutor1.jpg", "img/tutor2.jpg"];
+const comicPages = [
+  { image: "img/title.jpg", sound: null},
+  { image: "img/tutorial1.jpg", sound: "sound/effect1.mp3" },
+  { image: "img/tutorial2.jpg", sound: "sound/effect2.mp3" },
+  { image: "img/tutorial3.jpg", sound: "sound/effect3.mp3" },
+  { image: "img/tutorial4.jpg", sound: "sound/effect4.mp3" }
+];
 
 var currentPage = 0;
 
 $(document).ready(function() {
-  $("#next-arrow").click(nextPage);
-  $("#prev-arrow").click(prevPage);
+
+  function playSoundEffect(soundPath) {
+    var soundEffect = new Audio(soundPath);
+    soundEffect.currentTime = 0;
+    soundEffect.play();
+  }
+
+  function loadPage(pageIndex) {
+    document.getElementById("comic-image").src = comicPages[pageIndex].image;
+    playSoundEffect(comicPages[pageIndex].sound);
+
+    if (pageIndex === 0) {
+      $("#prev_img").hide();
+    } else {
+      $("#prev_img").show();
+    }
+
+    if (pageIndex === 4) {
+      $("#next_img").hide();
+    } else {
+      $("#next_img").show();
+    }
+  }
 
   function nextPage() {
-  if (currentPage < comicPages.length - 1) {
-    currentPage++;
-    document.getElementById("comic-image").src = comicPages[currentPage];
+    if (currentPage < comicPages.length - 1) {
+      currentPage++;
+      loadPage(currentPage);
+    }
   }
-}
 
-function prevPage() {
-  if (currentPage > 0) {
-    currentPage--;
-    document.getElementById("comic-image").src = comicPages[currentPage];
+  function prevPage() {
+    if (currentPage > 0) {
+      currentPage--;
+      loadPage(currentPage);
+    }
   }
-}
+
+  loadPage(currentPage);
+
+  $("#prev_img").on("click", prevPage);
+  $("#next_img").on("click", nextPage);
+
 });
+
