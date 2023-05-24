24e68b88e9863cc9c9d046771234148f84177af5
rlagkswn00 <hjkim2902@naver.com>
2023-05-24 17:07:39 +0900

    feat: 다음 라운드 넘어가도록 설정

--- Server_TeamProject/src/js/phase1.js
+++ Server_TeamProject/src/js/phase1.js
@@ -1,7 +1,9 @@
 $(init_game);
 
 var gameStatus; // 게임 상태
-var jsonData; // json data
+var settingData; // setting Data
+var playingData; // playing Data
+
 var difficulty; // 0,1,2 -> easy , normal, hard
 var life; // 라이프
 var score; // 점수
@@ -25,6 +27,7 @@ var is_gamewin = false;
 var difficulty_NROWS = [4, 5, 6];
 var difficulty_NCOLS = [4, 5, 6];
 
+var bgi = new Image(); // 배경 이미지
 var backgroundImgs = [
   "img/background/easy1.jpeg",
   "img/background/easy2.png",
@@ -39,6 +42,7 @@ var monsterImgs = [
   "img/monster/monster3.png",
   "img/monster/monster4.png",
 ];
+
 var paddlex; // 패들 위치 x
 var paddleh; // 패들 높이 y
 var paddlew; // 패들 너비 w
@@ -57,8 +61,7 @@ var PADDING; // 벽돌 간격
 var ctx; // 캔버스 컨텍스트
 var anim; // 애니메이션
 
-var bgi = new Image(); // 배경 이미지
-
+var bossHp = 10; // 보스 체력
 //////////////////////////////////////////////////
 //-------------------- draw---------------------//
 //////////////////////////////////////////////////
@@ -132,11 +135,6 @@ function init_paddle() {
   paddleh = 10;
 }
 function init_bgi(difficulty) {
-  // status 1, 2
-  // difficulty 1,2,3
-  // bgi 1,2,3,4,5,6
-  // 1-1, 1-2, 2-1, 2-2, 3-1, 3-2
-  //  1    2    3    4    5   6
   var level = 0;
   if (gameStatus == 1) level = 2 * difficulty - 1;
   else if (gameStatus == 2) level = 2 * difficulty;
@@ -178,19 +176,40 @@ function init_game() {
   canvasMaxX = canvasMinX + WIDTH;
 
   $.ajax({
-    url: "data/data.json",
+    url: "data/setting.json",
     dataType: "json",
-    success: function (data) {
-      jsonData = data;
-      gameStatus = jsonData[0].status;
-      difficulty = jsonData[0].difficulty;
-      life = jsonData[0].life;
-      score = jsonData[0].score;
-      damage = jsonData[0].damage;
-      radius = jsonData[0].radius;
-      paddlew = jsonData[0].paddlew;
-      init_html();
-      gameSwitch();
+    success: function (data1) {
+      settingData = data1;
+
+      $.ajax({
+        url: "data/play.json",
+        dataType: "json",
+        success: function (data2) {
+          playingData = data2;
+
+          //첫 게임
+          if (gameStatus == 1 && difficulty == 1) {
+            gameStatus = settingData[0].status;
+            difficulty = settigData[0].difficulty;
+            life = settingData[0].life;
+            score = settingData[0].score;
+            damage = settingData[0].damage;
+            radius = settingData[0].radius;
+            paddlew = settingData[0].paddlew;
+          } else {
+            //그외
+            gameStatus = playingData[0].status;
+            difficulty = playingData[0].difficulty;
+            life = playingData[0].life;
+            score = playingData[0].score;
+            damage = playingData[0].damage;
+            radius = playingData[0].radius;
+            paddlew = playingData[0].paddlew;
+          }
+          init_html();
+          gameSwitch();
+        },
+      });
     },
   });
 }
@@ -214,8 +233,13 @@ function updateBricks() {
   if (row < NROWS) {
     if (bricks[row][col] == 1) {
       dy = -dy;
+      var effectAudio = new Audio("sound/game/brickHit.mp3");
+      effectAudio.play();
       bricks[row][col] = 0;
     } else if (bricks[row][col] >= 2) {
+      var effectAudio = new Audio("sound/game/monsterHit.mp3");
+      effectAudio.play();
+      monsterCnt--;
       dy = -dy;
       updateItem(bricks[row][col] - 1);
       bricks[row][col] = 0;
@@ -223,42 +247,26 @@ function updateBricks() {
   }
 }
 function updateItem(idx) {
-  //TODO : item 으로 인한 효과
-  paddlew += 20;
-  jsonData[0].paddlew = paddlew;
-
-  //TODO : 데미지 증가
-  damage += 1;
-  jsonData[0].damage = damage;
-
-  //TODO : 라이프 추가
-  life += 1;
-  jsonData[0].life = life;
-
-  //TODO : 공 크기 증가
-  radius += 3;
-  jsonData[0].radius = radius;
-
   switch (idx) {
     case 1:
       //TODO : 바 길이 증가
       paddlew += 20;
-      jsonData[0].paddlew = paddlew;
+      playingData[0].paddlew = paddlew;
       break;
     case 2:
       //TODO : 데미지 증가
       damage += 1;
-      jsonData[0].damage = damage;
+      playingData[0].damage = damage;
       break;
     case 3:
       //TODO : 라이프 추가
       life += 1;
-      jsonData[0].life = life;
+      playingData[0].life = life;
       break;
     case 4:
       //TODO : 공 크기 증가
       radius += 3;
-      jsonData[0].radius = radius;
+      playingData[0].radius = radius;
       break;
     default:
       break;
@@ -287,12 +295,39 @@ function updateDirection() {
   }
 }
 function updateGameStatus(game) {
-  //게임 종료 조건
   if (is_gameover) {
     //게임 종료
     window.cancelAnimationFrame(anim);
   } else {
-    anim = window.requestAnimationFrame(game);
+    //잡몹전 일떄
+    if (gameStatus == 1) {
+      if (monsterCnt == 0) {
+        //잡몹전 우승
+        gameStatus = 2;
+        playingData[0].status = gameStatus;
+        gameSwitch();
+      } else {
+        anim = requestAnimationFrame(game);
+      }
+    } else {
+      //보스전 일때
+      if (bossHp == 0) {
+        //마지막 게임이면 끝
+        if (difficulty == 3) {
+          //TODO : 게임 끝
+          window.cancelAnimationFrame(anim);
+        } else {
+          //다음 게임
+          difficulty++;
+          playingData[0].difficulty = difficulty;
+          gameStatus = 1;
+          playingData[0].status = gameStatus;
+          gameSwitch();
+        }
+      } else {
+        anim = requestAnimationFrame(game);
+      }
+    }
   }
 }
 
@@ -365,4 +400,3 @@ function phase2() {
   updateDirection();
   updateGameStatus(phase2);
 }
-//TODO init level data.json에서 받아와야함.
