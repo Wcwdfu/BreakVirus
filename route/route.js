a2fcf397133e67e6718c47fd9ac068dab5082ccd
rlagkswn00 <hjkim2902@naver.com>
2023-05-25 21:01:22 +0900

    feat: 메인 라이팅 튜토리얼로 변경

--- route/route.js
+++ route/route.js
@@ -2,10 +2,10 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", function (req, res) {
-  res.redirect("src/phase1.html");
-});
-router.get("/t", function (req, res) {
   res.redirect("src/tutorial.html");
+});
+router.get("/p", function (req, res) {
+  res.redirect("src/phase1.html");
 });
 router.all("*", function (req, res) {
   res.status(404).send("<h1>ERROR - 페이지를 찾을 수 없습니다.</h1>");
