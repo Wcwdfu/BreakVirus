78b62b0949ab813b29b3ada143bca7d0a418e79e
ksk0823 <machine0823@naver.com>
2023-05-28 05:55:32 +0900

    메인 화면 완성
    how to play 이미지 파일 빼고 메인화면 완성 했습니다.

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,23 @@
 $(document).ready(function() {
 
+	document.body.style.backgroundImage = "url('img/mainImg/background2.png')";
+	document.body.style.backgroundSize = "cover";
+	document.body.style.backgroundRepeat = "no-repeat";
+
+	var position = 0;
+	var speed = 1; // 이동 속도 조정
+	var windowWidth = window.innerWidth; // 창의 가로 크기
+
+	function moveBackground() {
+		position -= speed;
+		if (position <= -windowWidth) {
+			position = 0;
+		}
+		document.body.style.backgroundPosition = position + "px 0";
+	}
+
+	setInterval(moveBackground, 20);
+
 	$("#start_btn img").hover(
 		function() {
 			$("#start_btn img").attr("src", "img/mainImg/start_2.png");
@@ -31,39 +49,74 @@ $(document).ready(function() {
         audio.play();
       }
 
+	// click시 효과음
+	function playClickSound() {
+        var audio = document.getElementById("click_sound");
+        audio.currentTime = 0;
+        audio.play();
+      }
+
 	// Start 버튼 클릭
 	$("#start_btn").click(function() {
+		playClickSound();
 		window.location.href = "phase1.html";
 	});
 
 	// Setting 버튼 클릭
 	$("#settings_btn").click(function() {
+		playClickSound();
 		$("#main").hide();
 		$("#setting_screen").show();
 	});
 
 	// How to play 버튼 클릭
 	$("#how_to_play_btn").click(function() {
+		playClickSound();
 		$("#main").hide();
 		$("#how_to_play_screen").show();
 	});
 
 	// 설정창에서 Confirm 버튼 클릭
-	$("#setting_confirm_btn").click(function() {
+	$("#settings_ok").click(function() {
+		playClickSound();
 		$("#setting_screen").hide();
 		$("#main").show();
 	});
 
 	// 설정창 화면에서 Back 버튼 클릭
-	$("#setting_back_btn").click(function() {
+	$("#settings_back").click(function() {
+		playClickSound();
 		$("#setting_screen").hide();
 		$("#main").show();
 	});
 
 	// How to play 화면에서 Back 버튼 클릭
-	$("#how_to_play_back_btn").click(function() {
+	$("#htp_back").click(function() {
+		playClickSound();
 		$("#how_to_play_screen").hide();
 		$("#main").show();
 	});
+
+	// BGM 체크 박스
+	$("#bgm_checkbox").change(function () {
+		if ($(this).prop("checked")) {
+			$("#bgm_checkbox_img").attr("src", "img/mainImg/bgm_checked.png");
+		} else {
+			$("#bgm_checkbox_img").attr("src", "img/mainImg/bgm_unchecked.png");
+		}
+	});
+  
+	// Infinite Life Mode 체크 박스
+	$("#infL_checkbox").change(function () {
+		if ($(this).prop("checked")) {
+			$("#infinite_checkbox_img").attr("src", "img/mainImg/infL_checked.png");
+		} else {
+			$("#infinite_checkbox_img").attr("src", "img/mainImg/infL_unchecked.png");
+		}
+	});
+
+	$('#left_character').css({ left: '-100%' }).animate({ left: '50' }, 1000);
+
+	$('#right_character').css({ right: '-100%' }).animate({ right: '50' }, 1000);
 	  
 });
